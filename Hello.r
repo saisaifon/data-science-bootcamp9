2d96089ef648e5473b68e2adc0285eab68c96105
saisaifon <167694159+saisaifon@users.noreply.github.com>
2024-04-22 00:29:28 +0700

    Create Hello.r

---
+++ Hello.r
@@ -0,0 +1,13 @@
+# my first R program
+
+print("hello world")
+print("oppenheimer is very good movie")
+
+library(dplyr)
+mtcars %>%
+  select(1:5) %>%
+  filter(mpg > 30)
+
+mtcars %>%
+  select(mpg) %>%
+  filter(mpg < 30)
